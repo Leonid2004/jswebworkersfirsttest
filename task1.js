70f5b7fea75f92933719d1c4626bb88e4e04a714
Leonid2004 <leophilo7@gmail.com>
2020-12-16 22:57:19 +0100

    13

--- task1.js
+++ task1.js
@@ -1,11 +1,6 @@
+// worker.js
 self.addEventListener('message', function(e) {
-  var data = e.data;
-  switch (data.cmd) {
-    case 'average':
-      var result = calculateAverage(data); // Функция, вычисляющая среднее значение числового массива
-      self.postMessage(result);
-      break;
-    default:
-      self.postMessage('Unknown command');
-  }
-}, false);
+  var message = e.data + 'to myself!';
+  self.postMessage(message);
+  self.close();
+}
